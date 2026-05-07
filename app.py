125eb5b7f5aa3ab60baa91fb6e45a53460a82848
pmontoya1914-lang <pmontoya1914@gmail.com>
2026-05-07 15:25:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-st.title("DIAGNÓSTICO EMPRESARIAL - ÁREA ADMINISTRATIVA/ORGANIZACIONAL")
+st.title("DIAGNÓSTICO EMPRESARIAL - REVISIÓN DONDE TE ENCUENTRAS HOY EN TU EMPRENDIMIENTO")
 st.write("Este cuestionario tiene como objetivo evaluar el nivel de madurez de un emprendimiento en seis áreas clave: legal, administrativa, comercial, financiera, técnica y ecológica. No existen respuestas correctas o incorrectas. Responde con honestidad para obtener un diagnóstico útil.")
 st.write("Escala: 1 = No existe, 2 = Existe de manera informal, 3 = Existe parcialmente, 4 = Existe y funciona adecuadamente, 5 = Existe y está documentado\n")
 
 total_admin = 0
 import streamlit as st
-total_admin += st.selectbox("1. ¿Cuenta con estructura organizacional definida?", [1,2,3,4,5])
-total_admin += st.selectbox("2. ¿Están claros los roles y responsabilidades?", [1,2,3,4,5])
+total_admin += st.selectbox("1. ¿Cuenta con estructura organizacional definida?", [0,1,2,3,4,5])
+total_admin += st.selectbox("2. ¿Están claros los roles y responsabilidades?", [0,1,2,3,4,5])
 total_admin += st.selectbox("3. ¿Existe un responsable de decisiones clave?", [1,2,3,4,5])
 total_admin += st.selectbox("4. ¿Los socios tienen funciones diferenciadas?", [1,2,3,4,5])
 total_admin += st.selectbox("5. ¿Las decisiones se documentan?", [1,2,3,4,5])
